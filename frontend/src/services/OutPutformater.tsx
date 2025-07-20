8d7322b28d33332eea5f09cbd656c2a203a1ea6b
Vipul Yadav <123956067+Vipul-045@users.noreply.github.com>
2025-07-20 13:20:46 +0530

    Update OutPutformater.tsx

--- frontend/src/services/OutPutformater.tsx
+++ frontend/src/services/OutPutformater.tsx
@@ -238,7 +238,7 @@ const FormulaMatch = (chuckData: string): string => {
 
     if (match) {
         const latextData = myConvertLatex(match[1]);
-        chuckData = chuckData.replace(/(\\[\\[\\(])\s*([\s\S]*?)\s*(\\[\]\\)])/, (match, open, inner, close) => {
+        chuckData = chuckData.replace(/(\\[\\[\\(])\s*([\s\S]*?)\s*(\\[\]\\)])/, ( open, close) => {
             return `${open} ${latextData} ${close}`;
         });
     }
